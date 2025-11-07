2350192ff75bdb30cd0e8c6ce1d543680f242576
LILCODAH <rajkumar14ailawadi@gmail.com>
2025-11-08 03:41:59 +0530

    formatting

--- Models/userModel.js
+++ Models/userModel.js
@@ -2,15 +2,15 @@ const mongoose = require("mongoose");
 const {Schema , model} = mongoose
 
 const userSchema = new Schema({
-  name: { type: String,
-    required: true 
+    name: { type: String,
+        required: true 
     },
-  email: { type: String, 
-    required: true, 
-    unique: true 
+    email: { type: String, 
+        required: true, 
+        unique: true 
     },
-  password: { type: String, 
-    required: true 
+    password: { type: String, 
+        required: true 
     },
 }, { timestamps: true });
 
