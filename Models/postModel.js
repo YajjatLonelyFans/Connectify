4a42ab53e3a5a0ddcb4e89427a330ec6428521c2
LILCODAH <rajkumar14ailawadi@gmail.com>
2025-11-08 03:31:25 +0530

    models ready

---
+++ Models/postModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+const {Schema , model} = mongoose
+const postSchema = new Schema({
+    userId: {
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    content:{
+        type:String,
+        required:true
+    }
+}, {timestamps:true})
+
+module.exports = model("Post" , postSchema)
