5da7474bb8ca06d480723f40bb432daeda9da733
LILCODAH <rajkumar14ailawadi@gmail.com>
2025-11-08 04:21:00 +0530

    UserControllers JWT Bcrypt Auth and moved model folder

---
+++ Backend/Controllers/userControllers.js
@@ -0,0 +1,35 @@
+const User = require("../Models/userModel.js")
+const jwt = require("jsonwebtoken");
+
+exports.registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(400).json({ message: "User already exists" });
+
+    const newUser = await User.create({ name, email, password });
+
+    res.status(201).json({ message: "User registered successfully", user: newUser });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid email or password" });
+
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch) return res.status(400).json({ message: "Invalid email or password" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "3d" });
+
+    res.status(200).json({ message: "Login successful", token, user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
---
+++ Backend/Models/userModel.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const {Schema , model} = mongoose
+
+const userSchema = new Schema({
+    name: { 
+        type: String,
+        required: true 
+    },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    password: { 
+        type: String, 
+        required: true 
+    },
+}, { timestamps: true });
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+
+module.exports = model("User", userSchema);
