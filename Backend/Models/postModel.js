427548759193263bee98c8528fc3f91ca39c8f84
LILCODAH <rajkumar14ailawadi@gmail.com>
2025-11-09 23:30:23 +0530

    My dumbah forgot to add title in schema

--- Backend/Models/postModel.js
+++ Backend/Models/postModel.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose")
 const {Schema , model} = mongoose
 const postSchema = new Schema({
+    title:{
+        type:String,
+        required:true
+    },
     user: {
         type:Schema.Types.ObjectId,
         ref:"User",
