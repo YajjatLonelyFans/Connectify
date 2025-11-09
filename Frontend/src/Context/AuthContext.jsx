80e23c99c821ba0f6a5d6dd2654f61363933c687
LILCODAH <rajkumar14ailawadi@gmail.com>
2025-11-10 02:13:00 +0530

    deploying frontend

--- Frontend/src/Context/AuthContext.jsx
+++ Frontend/src/Context/AuthContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState, useEffect } from "react";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = "https://connectify-r0y3.onrender.com";
 
 
 const AuthContext = createContext();
